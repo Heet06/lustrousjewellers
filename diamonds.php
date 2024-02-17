62f0ff2b293aaa14fbec8b538124b8a9d547785d
Heet06 <radadiyaheet@gmail.com>
2024-02-17 15:41:42 +0530

    fixed an error.

--- diamonds.php
+++ diamonds.php
@@ -88,7 +88,7 @@ $exe = mysqli_query($con, $query);
         </div>
     </div>
     </div>
-    <?php include 'footer.php'?>
+    <?php include 'components/footer.php'?>
     <script src="https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.js"></script>
     <script>
     const scroll = new LocomotiveScroll({
