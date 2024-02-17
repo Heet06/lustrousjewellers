9b457d423207623b0464c675f960e0ff356b47e1
Heet06 <radadiyaheet@gmail.com>
2024-02-17 16:34:38 +0530

    expanded animation to index page.

--- index.php
+++ index.php
@@ -338,7 +338,6 @@ $exe = mysqli_query($con, $query);
       </div>
     </div>
   </div>
-  </div>
   <?php include 'components/footer.php'; ?>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
     integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
