484734d017bfd681f713c60abd0949dd6fcbcaf5
Heet06 <radadiyaheet@gmail.com>
2024-02-18 12:50:21 +0530

    reduced the gap between card.

--- ornaments.php
+++ ornaments.php
@@ -42,7 +42,7 @@ $exe = mysqli_query($con, $query);
             while ($row = mysqli_fetch_array($exe)) {
                 $src = explode(',', $row['images']);
                 ?>
-                <div class="col-sm-6 col-lg-4 mb-4" data-scroll data-scroll-speed="2">
+                <div class="col-sm-6 col-lg-4 mb-2" data-scroll data-scroll-speed="2">
                     <div class="card">
                         <div id="carousel-<?php echo $row['token']; ?>" class="carousel slide" data-bs-ride="carousel">
                             <div class="carousel-inner">
