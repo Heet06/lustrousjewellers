34ec2f2c28b08cb0da71768dc2b9b1c3efb84c19
Heet06 <radadiyaheet@gmail.com>
2024-02-18 12:36:58 +0530

    bug fix.

--- ornaments.php
+++ ornaments.php
@@ -132,15 +132,6 @@ $exe = mysqli_query($con, $query);
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        var $container = $('.row');            
-        $container.imagesLoaded(function(){                 
-        $container.masonry({
-            itemSelector: 'col-sm-6.col-lg-4.mb-4',
-            isAnimated: true,
-        });
-        });
-    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"
         integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
@@ -151,6 +142,15 @@ $exe = mysqli_query($con, $query);
     <script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js"
         integrity="sha512-JRlcvSZAXT8+5SQQAvklXGJuxXTouyq8oIMaYERZQasB8SBDHZaUbeASsJWpk0UUrf89DP3/aefPPrlMR1h1yQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        var $container = $('.row');            
+        $container.imagesLoaded(function(){                 
+        $container.masonry({
+            itemSelector: 'col-sm-6.col-lg-4.mb-4',
+            isAnimated: true,
+        });
+        });
+    </script>
     <script src="assets/js/Animated-Pretty-Product-List-animated-column.js"></script>
     <script src="assets/js/Drag-and-Drop-Multiple-File-Form-Input-upload-Advanced-drop.js"></script>
     <script src="assets/js/theme.js"></script>
