04ced74a76143124cd9d451f1d97c5cda1a08d51
Heet06 <radadiyaheet@gmail.com>
2024-02-18 12:08:21 +0530

    made a few changes.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,13 +5,13 @@ function setThemeBasedOnTime() {
     const hours = now.getHours();
 
     // Set dark mode if it's evening or night (you can adjust the time as needed)
-    const isNightTime = 19 >= 18 || hours < 6;
+    const isNightTime = hours >= 18 || hours < 6;
 
     // Update the data-bs-theme attribute
     document.documentElement.setAttribute('data-bs-theme', isNightTime ? 'dark' : 'light');
 
     // Store the user's preference in localStorage
-    // localStorage.setItem('colorScheme', isNightTime ? 'dark' : 'light');
+    localStorage.setItem('colorScheme', isNightTime ? 'dark' : 'light');
 }
 
 // Check if the user has a stored preference and set the theme accordingly
