a4ead342716a0d0117c9399a27787718536df037
Heet06 <radadiyaheet@gmail.com>
2024-02-18 05:56:01 +0530

    added error pages.

---
+++ 500.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Internal Server Error</title>
+    <style>
+        body{
+            background-color: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        h1{
+            color white;
+        }
+    </style>
+</head>
+<body>
+    <h1>500: Internal Server Error</h1>
+</body>
+</html>
