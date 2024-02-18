2330f08eac084e5da7867832f8742b68e66698ff
Heet06 <radadiyaheet@gmail.com>
2024-02-18 14:34:42 +0530

    added an error page.

--- error.php
+++ error.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Some Error Occured</title>
     <style>
-        body{
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        html, body{
             height: 100%;
             width: 100%;
             background-color: black;
@@ -14,12 +19,30 @@
             align-items: center;
         }
 
-        h1{
+        #errorDiv{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            margin: 10px;
+            padding: 20vh;
+            border-radius: 1.25rem;
+            background-color: rgb(255 255 255 / 10%);
+            backdrop-filter: blur(10px);
             color: white;
+            font-size: 4vh;
         }
     </style>
 </head>
 <body>
-    <h1>Sorry! There's some error</h1>
+    <div id="errorDiv">
+        <p>Sorry! There's some error</p><br>
+        <p>Redirecting to Home Page</p>
+    </div>
+    <script>
+        setTimeout(function() {
+            window.open('/', '_self');
+        }, 5000);
+    </script>
 </body>
 </html>
