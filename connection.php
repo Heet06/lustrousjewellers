279a08b120f48ded8ee883c035b9e1270793feb6
Heet06 <radadiyaheet@gmail.com>
2024-05-31 21:22:39 +0530

    added error safe db connection code.

--- connection.php
+++ connection.php
@@ -1,11 +1,24 @@
 <?php
-$user = "if0_35586370";
-$password = "0p6n4BTuepZhTG";
-$server = "sql311.infinityfree.com";
-$db = "if0_35586370_lustrousjewellers";
+try {
+    $user = "if0_35586370";
+    $password = "0p6n4BTuepZhTG";
+    $server = "sql311.infinityfree.com";
+    $db = "if0_35586370_lustrousjewellers";
 
-$con = mysqli_connect($server, $user, $password, $db);
+    $con = mysqli_connect($server, $user, $password, $db);
 
-if (!$con) {
-    echo "<script>alert('Connection Failed');</script>";
+    if (!$con) {
+        echo "<script>alert('Connection Failed');</script>";
+    }
+} catch (\Throwable $th) {
+    $user = "root";
+    $password = "";
+    $server = "localhost";
+    $db = "lustrousjewellers";
+
+    $con = mysqli_connect($server, $user, $password, $db);
+
+    if (!$con) {
+        echo "<script>alert('Connection Failed');</script>";
+    }
 }
